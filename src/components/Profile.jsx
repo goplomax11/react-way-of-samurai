110cc88c473befb3a70d11912cc0efa33268184b
goplomax11 <62140181+goplomax11@users.noreply.github.com>
2020-09-20 18:01:44 +0300

    sda

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 import s from './Profile.module.css';
 
@@ -27,6 +28,36 @@ const Profile = () => {
   </div>
 }
 
+=======
+import React from 'react';
+import s from './Profile.module.css';
+
+const Profile = () => {
+    return <div className={s.content}>
+    <div>
+      <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div className={s.posts}>
+        <div className={s.item}>
+          post 1
+        </div>
+        <div className='item'>
+          post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+>>>>>>> parent of f21190a... 15: posts, post component; folder structure
 =======
 import React from 'react';
 import s from './Profile.module.css';
